190e65d5df09602ba94a4d530122648c3e281de0
Jaeyoung <jaeyoungshin03@gmail.com>
2022-02-10 11:19:34 +0700

    fix

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxhealth = 100;
+    public int currentHealth = 100;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        healthBar.setMaxHealth(maxhealth);
+    }
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D other) {
+        int damage = other.gameObject.GetComponent<Bullet>().bulletDamage;
+        currentHealth -= damage;
+        healthBar.setHealth(currentHealth);
+
+    }
+
+}
